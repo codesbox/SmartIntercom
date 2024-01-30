b4b49855392e0579afc996f62dec7cc61956b61c
Yasashnyy Maxim <max.yas@list.ru>
2024-01-30 10:29:52 +0300

    fix: fix bug

--- app/src/main/java/ru/samsung/smartintercom/ui/screen/callhistory/CallHistoryScreen.kt
+++ app/src/main/java/ru/samsung/smartintercom/ui/screen/callhistory/CallHistoryScreen.kt
@@ -11,11 +11,13 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import org.koin.androidx.compose.koinViewModel
+import ru.samsung.smartintercom.R
 import ru.samsung.smartintercom.R.string
 import ru.samsung.smartintercom.core.CallHistoryId
 import ru.samsung.smartintercom.domain.callHistory.models.CallInfo
@@ -27,48 +29,60 @@ import ru.samsung.smartintercom.utils.setupScreenData
 
 object CallHistoryScreen : ScreenBaseData {
     override val name = CallHistoryId.screenId
-    
+
     @OptIn(ExperimentalMaterial3Api::class)
     @Composable
     override fun Render(navController: NavController) {
         val viewModel = koinViewModel<CallHistoryViewModel>()
         val calls by viewModel.calls.collectAsState()
-        
+
         LaunchedEffect(key1 = Unit, block = {
             viewModel.getHistory()
         })
-        
+
+
         Scaffold(modifier = Modifier.setupScreenData(this)) { paddingValues ->
-            when (calls.isEmpty()) {
-                true  -> Box(modifier = Modifier.fillMaxSize()) {
-                    Text(
-                        modifier = Modifier.align(Alignment.Center),
-                        text = stringResource(string.no_calls_yet),
-                        style = MaterialTheme.typography.displaySmall,
-                        textAlign = TextAlign.Center,
+            Column(modifier = Modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.SpaceEvenly) {
+                IconButton(modifier = Modifier
+                    .align(Alignment.Start)
+                    .padding(8.dp) ,onClick = { navController.popBackStack() }) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.back),
+                        contentDescription = stringResource(string.go_back)
                     )
                 }
-                
-                false -> LazyColumn(
-                    modifier = Modifier
-                        .testTag(CallHistoryId.recycler)
-                        .fillMaxSize()
-                        .padding(paddingValues)
-                ) {
-                    items(calls) {
-                        CallInfoCard(callInfo = it)
+                when (calls.isEmpty()) {
+                    true -> Box(modifier = Modifier.fillMaxSize()) {
+                        Text(
+                            modifier = Modifier.align(Alignment.Center),
+                            text = stringResource(string.no_calls_yet),
+                            style = MaterialTheme.typography.displaySmall,
+                            textAlign = TextAlign.Center,
+                        )
+                    }
+
+                    false -> LazyColumn(
+                        modifier = Modifier
+                            .testTag(CallHistoryId.recycler)
+                            .fillMaxSize()
+                            .padding(paddingValues)
+                    ) {
+                        items(calls) {
+                            CallInfoCard(callInfo = it)
+                        }
                     }
                 }
             }
         }
     }
-    
+
     @Composable
     fun CallInfoCard(callInfo: CallInfo) {
         Card(
             modifier = Modifier
                 .wrapContentWidth()
-                .wrapContentHeight().padding(12.dp)
+                .wrapContentHeight()
+                .padding(12.dp)
         ) {
             Row(
                 modifier = Modifier
@@ -86,7 +100,7 @@ object CallHistoryScreen : ScreenBaseData {
                 Text(
                     modifier = Modifier.testTag(CallHistoryId.textStatus),
                     text = when (callInfo.doorState) {
-                        OPEN  -> stringResource(string.opened)
+                        OPEN -> stringResource(string.opened)
                         CLOSE -> stringResource(string.closed)
                     },
                     style = MaterialTheme.typography.headlineLarge
@@ -94,6 +108,6 @@ object CallHistoryScreen : ScreenBaseData {
             }
         }
     }
-    
+
     private fun CallTime.convertToString() = "$hours:$minutes $day.$month.$year"
 }
